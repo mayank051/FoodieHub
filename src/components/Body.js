8fadb3399b50ae7af713ad016d91f3f757a7526c
Mayank <mayankraj051@gmail.com>
2024-08-06 17:30:07 +0530

    Added restaurant filter capability

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,27 @@
+import { useState } from "react";
 import Card from "./Card";
 import { respData } from "../mocks/mockData";
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(respData);
   return (
     <div className="body">
       <div className="search-bar">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.card.card.info.avgRating > 4.3
+            );
+            console.log(filteredList);
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {respData.map((resp) => {
+        {listOfRestaurants.map((resp) => {
           const restaurantData = resp.card.card.info;
           return <Card resData={restaurantData} key={restaurantData.id} />;
         })}
