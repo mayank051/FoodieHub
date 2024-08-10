b88218d17c5b819283a7ab33006df5ce2e27ecc4
Mayank <mayankraj051@gmail.com>
2024-08-10 12:27:01 +0530

    Added API call to fetch restaurants

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Card from "./Card";
 import { respData } from "../mocks/mockData";
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState(respData);
+
+  useEffect(() => {
+    fetchRestaurants();
+  }, []);
+
+  const fetchRestaurants = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.8389444&lng=77.6898209&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const jsonData = await data.json();
+    console.log("Data-->", jsonData);
+    // setListOfRestaurants(jsonData.data);
+  };
   return (
     <div className="body">
       <div className="search-bar">Search</div>
